3a792a3ae7259427967d7f01347646b21a5b5ddd
Rushani Shawuti <ruhsane924@gmail.com>
2019-04-10 16:55:44 -0700

    keychain pod added and basic ui done

--- Keychain/Keychain/ViewController.swift
+++ Keychain/Keychain/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBOutlet weak var syncSwitch: UISwitch!
+    
+    @IBOutlet weak var outputTextView: UITextView!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    @IBAction func saveClicked(_ sender: Any) {
+    }
+    
+    @IBAction func deleteClicked(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- UnitTest/UnitTestTests/UnitTestTests.swift
+++ UnitTest/UnitTestTests/UnitTestTests.swift
@@ -66,3 +66,4 @@ class UnitTestTests: XCTestCase {
         
     }
 }
+
