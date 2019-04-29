23bfffed8b244364d818d45850ab4613c01d2384
Rushani Shawuti <ruhsane924@gmail.com>
2019-04-29 16:11:39 -0700

    three unit test and passes

---
+++ Keychain/KeychainTests/KeychainTests.swift
@@ -0,0 +1,55 @@
+//
+//  KeychainTests.swift
+//  KeychainTests
+//
+//  Created by Ruhsane Sawut on 4/29/19.
+//  Copyright © 2019 ruhsane. All rights reserved.
+//
+
+import XCTest
+@testable import Keychain
+import KeychainSwift
+
+class KeychainTestingTests: XCTestCase {
+    
+    // For all of the tests we will need an instance of KeychainSwift
+    let keychain = KeychainSwift()
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    
+    func testKeychainGet() {
+        // Test if retrieving a value with Keychain works.
+        keychain.set("value", forKey: "key")
+        let string = keychain.get("key")
+        XCTAssertEqual(string, "value")
+        
+    }
+    
+    func testKeychainDelete() {
+        // Test if deleting a value with Keychain works.
+        keychain.set("value", forKey: "key")
+        keychain.delete("key")
+        let get = keychain.get("key")
+        XCTAssertEqual(get, nil)
+        
+    }
+    
+    func testKeychainUpdate() {
+        // Test if updating a value with Keychain works, you will need an initial value and compare the new value with the old one.
+        keychain.set("value", forKey: "key")
+        let old = keychain.get("key")
+        XCTAssertEqual(old, "value")
+        
+        keychain.set("new", forKey: "key")
+        let new = keychain.get("key")
+        XCTAssertEqual(new, "new")
+
+    }
+}
