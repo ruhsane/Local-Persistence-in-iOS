e4f97c362375d6840ce930cb30d7b2172b2850e8
Rushani Shawuti <ruhsane924@gmail.com>
2019-04-29 17:34:30 -0700

    implemented tearDown

--- coredata-tests/ToDoTests/ToDoTests.swift
+++ coredata-tests/ToDoTests/ToDoTests.swift
@@ -65,7 +65,7 @@ class ToDoTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         // TODO: Clear all mock data
-
+        clearData()
         super.tearDown()
     }
     
